b68017b6c66518ae7f3e52f5a1013ac19de594f7
KODAK <linhtt.it@gmail.com>
2019-10-28 12:43:32 +0700

    04 - Activity Navigation for BottomNavigationView

--- app/src/main/java/com/example/instagramclone/ShareActivity.java
+++ app/src/main/java/com/example/instagramclone/ShareActivity.java
@@ -1,8 +1,9 @@
 package com.example.instagramclone;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,14 +11,12 @@ import android.view.MenuItem;
 import com.example.instagramclone.utils.BottomNavigationViewHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
-public class HomeActivity extends AppCompatActivity {
+public class ShareActivity extends AppCompatActivity {
+	private static final String TAG = "ShareActivity";
+	private static final int ACTIVITY_NUM = 2;
+	private Context mContext = ShareActivity.this;
 
-	private static final String TAG = "HomeActivity";
-	private static final int ACTIVITY_NUM = 0;
-	private Context mContext = HomeActivity.this;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	@Override protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
 		Log.d(TAG, "onCreate start");
--- app/src/main/java/com/example/instagramclone/utils/BottomNavigationViewHelper.java
+++ app/src/main/java/com/example/instagramclone/utils/BottomNavigationViewHelper.java
@@ -1,5 +1,17 @@
 package com.example.instagramclone.utils;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+
+import com.example.instagramclone.HomeActivity;
+import com.example.instagramclone.LikesActivity;
+import com.example.instagramclone.ProfileActivity;
+import com.example.instagramclone.R;
+import com.example.instagramclone.SearchActivity;
+import com.example.instagramclone.ShareActivity;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class BottomNavigationViewHelper {
@@ -10,5 +22,35 @@ public class BottomNavigationViewHelper {
 		bottomNavigationViewEx.enableItemShiftingMode(false);
 		bottomNavigationViewEx.enableItemShiftingMode(false);
 		bottomNavigationViewEx.setTextVisibility(false);
+	}
+
+	public static void enableNavigation(final Context context, BottomNavigationViewEx view) {
+		view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+			@Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+				switch (item.getItemId()) {
+					case R.id.ic_house:
+						Intent intent1 = new Intent(context, HomeActivity.class);
+						context.startActivity(intent1);
+						break;
+					case R.id.ic_search:
+						Intent intent2 = new Intent(context, SearchActivity.class);
+						context.startActivity(intent2);
+						break;
+					case R.id.ic_circle:
+						Intent intent3 = new Intent(context, ShareActivity.class);
+						context.startActivity(intent3);
+						break;
+					case R.id.ic_alert:
+						Intent intent4 = new Intent(context, LikesActivity.class);
+						context.startActivity(intent4);
+						break;
+					case R.id.ic_android:
+						Intent intent5 = new Intent(context, ProfileActivity.class);
+						context.startActivity(intent5);
+						break;
+				}
+				return false;
+			}
+		});
 	}
 }
