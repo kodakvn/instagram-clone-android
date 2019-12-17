e4afec250b928cc8a7d750aba32347f3f2962ac7
KODAK <linhtt.it@gmail.com>
2019-12-17 20:26:37 +0700

    11 - Account Settings Navigation (2/2)

--- app/src/main/java/com/example/instagramclone/profile/AccountSettingsActivity.java
+++ app/src/main/java/com/example/instagramclone/profile/AccountSettingsActivity.java
@@ -1,9 +1,13 @@
 package com.example.instagramclone.profile;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.example.instagramclone.R;
@@ -13,11 +17,25 @@ import java.util.ArrayList;
 public class AccountSettingsActivity extends AppCompatActivity {
     private static final String TAG = "AccountSettingsActivity";
 
+    private Context mContext;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_settings);
+        mContext = AccountSettingsActivity.this;
         Log.d(TAG, "onCreate: AccountSettingsActivity");
+
+        setupSettingsList();
+
+        ImageView backArrow = (ImageView) findViewById(R.id.backArrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "back to Profile activity");
+                finish();
+            }
+        });
     }
 
     private void setupSettingsList() {
@@ -27,5 +45,8 @@ public class AccountSettingsActivity extends AppCompatActivity {
         ArrayList<String> options = new ArrayList<>();
         options.add(getString(R.string.edit_profile));
         options.add(getString(R.string.sign_out));
+
+        ArrayAdapter adapter = new ArrayAdapter(mContext, android.R.layout.simple_list_item_1, options);
+        listView.setAdapter(adapter);
     }
 }
