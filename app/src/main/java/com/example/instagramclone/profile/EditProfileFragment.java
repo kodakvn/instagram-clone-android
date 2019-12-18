ac29403ab9ff892d5f905559971d56b0eb3016aa
KODAK <linhtt.it@gmail.com>
2019-12-19 02:58:23 +0700

    14 - Universal Image Loader Config (EditProfileFragment)

--- app/src/main/java/com/example/instagramclone/profile/EditProfileFragment.java
+++ app/src/main/java/com/example/instagramclone/profile/EditProfileFragment.java
@@ -7,17 +7,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.instagramclone.R;
+import com.example.instagramclone.utils.UniversalImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class EditProfileFragment extends Fragment {
     private static final String TAG = "EditProfileFragment";
+
+    private ImageView mProfilePhoto;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
 
+        mProfilePhoto = (ImageView) view.findViewById(R.id.profile_photo);
+
+        initImageLoader();
+        setProfileImage();
+
         return view;
     }
+
+    private void initImageLoader() {
+    	UniversalImageLoader loader = new UniversalImageLoader(getActivity());
+		ImageLoader.getInstance().init(loader.getCongiguration());
+	}
+
+    private void setProfileImage() {
+		 String imageUrl = "crackberry.com/sites/crackberry.com/files/topic_images/2013/ANDROID.png";
+		 UniversalImageLoader.setImage(imageUrl, mProfilePhoto, null, "https://");
+	 }
 }
