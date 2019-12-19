630c9456308854842bb23a193ecc405c0bf73ff3
KODAK <linhtt.it@gmail.com>
2019-12-20 01:17:03 +0700

    18 - Register Layout

---
+++ app/src/main/java/com/example/instagramclone/login/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.instagramclone.login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import com.example.instagramclone.R;
+
+public class LoginActivity extends AppCompatActivity {
+	private static final String TAG = "LoginActivity";
+
+	@Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		Log.d(TAG, "onCreate start");
+	}
+}
